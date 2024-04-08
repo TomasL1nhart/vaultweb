fd9c05381040f23ddbe37c5db26587ce3b27c495
TomasL1nhart <tomlinhart9584@gmail.com>
2024-04-08 11:25:46 +0200

    webin

---
+++ disk.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Nimbus+Sans+L:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/styles2.css">
+    <title>Disk</title>
+    <link rel="icon" type="image/x-icon" href="img/main.ico">
+</head>
+<body>
+    <div class="container">
+        <div class="bgup">
+            <input type="checkbox" id="hamburger">
+            <label for="hamburger">
+                &#9776;
+            </label>
+            <nav>
+            <div class="menu">
+                <div><a href="index.html">Domů</a></div>
+                <div><a href="disk.html">Disk</a></div>
+                <div><a href="upload.html">Nahrát Model</a></div>
+            </div>
+            </nav>
+            <div class="button"> <a href="upload.html">Nahrát Model</a> </div>
+        </div>
+        <hr>
+        <h3>Tvůj Disk</h3>
+    </div>
+</body>
+</html>
---
+++ upload.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Nimbus+Sans+L:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/styles2.css">
+    <title>Upload</title>
+    <link rel="icon" type="image/x-icon" href="img/main.ico">
+</head>
+<body>
+    <div class="container">
+        
+
+    </div>
+
+</body>
+</html>
