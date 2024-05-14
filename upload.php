0bbccab34ad5c5e9064225ab12f34f5117d1e627
TomasL1nhart <tomlinhart9584@gmail.com>
2024-05-14 13:39:21 +0200

    vault

--- upload.php
+++ upload.php
@@ -1,21 +1,39 @@
 <?php
 include('config.php');
-if(isset($_POST['submit'])) {
-$file_name = $_FILES['image']['name'];
-$tempname = $_FILES['image']['tmp_name'];
-$folder = 'Uploads/'.$file_name;
 
-$query = mysqli_query($con, "Insert into models (file) values ('$file_name')");
-if(move_uploaded_file($tempname, $folder)) {
-echo "<h2>File uploaded successfully</h2>"; 
-} else {
-echo "<h2>File uploaded successfully</h2>";
-}
+if(isset($_POST['submit'])) {
+    $file_name = $_FILES['model']['name'];
+    $tempname = $_FILES['model']['tmp_name'];
+    $folder = 'Uploads/' . basename($file_name);
+    
+    $allowed_extensions = array('obj', 'fbx', 'glbf');
+    $file_extension = pathinfo($file_name, PATHINFO_EXTENSION);
+    if(!in_array(strtolower($file_extension), $allowed_extensions)) {
+        echo "<h4>Toto nejde nahrát, špatný formát. Jenom OBJ, FBX, GLBF soubory jsou povoleny.</h4>";
+        exit;
+    }
+
+    if ($_FILES['model']['error'] !== UPLOAD_ERR_OK) {
+        echo "<h4>An error occurred during file upload. Please try again.</h4>";
+        exit;
+    }
+
+    if(move_uploaded_file($tempname, $folder)) {
+        $query = mysqli_query($con, "INSERT INTO models (file) VALUES ('$file_name')");
+        if ($query) {
+            echo "<h4>File uploaded successfully and database updated.</h4>";
+        } else {
+            echo "<h4>Failed to update the database.</h4>";
+        }
+    } else {
+        echo "<h4>Failed to upload the file. Please check the folder permissions.</h4>";
+    }
 }
 ?>
 
 <!DOCTYPE html>
-<html lang="en"><head>
+<html lang="en">
+<head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/css2?family=Nimbus+Sans+L:wght@400;700&display=swap" rel="stylesheet">
@@ -33,7 +51,7 @@ echo "<h2>File uploaded successfully</h2>";
         <div class="image-container">
             <img src="img/upload.svg" alt="uploading" id="image" />
             <form id="uploadForm" method="post" enctype="multipart/form-data">
-              <input type="file" name="image" id="file-input" />
+              <input type="file" name="model" id="file-input" />
               <button type="submit" name="submit">Submit</button>
             </form>
           </div>
